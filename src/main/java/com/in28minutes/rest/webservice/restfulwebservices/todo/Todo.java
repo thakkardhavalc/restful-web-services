5fb68d75613a2d10fd0a0d4d71f63efc9fd3de4c
thakkardhavalc <thakkardhavalc@gmail.com>
2024-01-04 10:52:33 -0700

    added security dependency also added todo pojo and service class

---
+++ src/main/java/com/in28minutes/rest/webservice/restfulwebservices/todo/Todo.java
@@ -0,0 +1,40 @@
+package com.in28minutes.rest.webservice.restfulwebservices.todo;
+
+/**
+ * Created By dhhaval thakkar on 2024-01-04
+ */
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Todo {
+
+    private int id;
+    private String username;
+    private String description;
+    private LocalDate targetDate;
+    private boolean done;
+
+    public Todo() {
+
+    }
+
+    public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo [id=" + id + ", username=" + username + ", description=" + description + ", targetDate="
+                + targetDate + ", done=" + done + "]";
+    }
+
+}
